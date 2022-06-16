2a9b14ed6aa2ed1953ee5066549232261d89139e
M.CEM KÜTÜKCÜ <se.muhsincem@gmail.com>
2022-06-16 16:59:46 +0300

    Added Repository App

---
+++ RepositoryApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using Repository.Concrete;
+
+
+CarRepository carRepository = new CarRepository();
+
+Console.WriteLine(JsonConvert.SerializeObject(carRepository.GetAll()));
