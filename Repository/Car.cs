fefb0e6cd2493d15bd3339347b2bbb07e37de527
M.CEM KÜTÜKCÜ <se.muhsincem@gmail.com>
2022-06-16 16:57:10 +0300

    Added Repository Pattern

---
+++ Repository/Car.cs
@@ -0,0 +1,15 @@
+﻿using Repository.Abstract;
+
+namespace Repository
+{
+    public class Car : IEntity
+    {
+        public Guid  Id { get; set; }
+        public string Name { get; set; }
+        public Car(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
---
+++ Repository/Concrete/CarRepository.cs
@@ -0,0 +1,29 @@
+﻿using Repository.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Concrete
+{
+    public class CarRepository : IRepository<Car>
+    {
+        private List<Car> cars = new List<Car>();
+        public CarRepository()
+        {
+            cars.Add(new Car(Guid.NewGuid(),"BMW"));
+            cars.Add(new Car(Guid.NewGuid(),"Mercedes"));
+            cars.Add(new Car(Guid.NewGuid(),"Audi"));
+        }
+        public Car Get()
+        {
+            return cars.First();
+        }
+
+        public List<Car> GetAll()
+        {
+            return cars;
+        }
+    }
+}
