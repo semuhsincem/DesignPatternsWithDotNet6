8381c9880dbf8ad3a9165383d9b7b3acbe5a09ca
M.CEM KÜTÜKCÜ <se.muhsincem@gmail.com>
2022-06-17 09:03:16 +0300

    Added Proxy Pattern

---
+++ Proxy/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    public class Math
+    {
+        public double Add(double x, double y) => x + y;
+        public double Sub(double x, double y) => x - y;
+        public double Mul(double x, double y) => x * y;
+        public double Div(double x, double y) => x / y;
+    }
+}
---
+++ ProxyApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Proxy;
+
+Console.WriteLine("Hello, World!");
+
+// Create math proxy
+MathProxy proxy = new MathProxy();
+// Do the math
+Console.WriteLine("4 + 2 = " + proxy.Add(4, 2));
+Console.WriteLine("4 - 2 = " + proxy.Sub(4, 2));
+Console.WriteLine("4 * 2 = " + proxy.Mul(4, 2));
+Console.WriteLine("4 / 2 = " + proxy.Div(4, 2));
+// Wait for user
+Console.ReadKey();
